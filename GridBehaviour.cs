90dc040d00db4430a9dea732d218221cdec42eaf
zach <felix.reinemann@gmail.com>
2022-11-03 19:14:34 +0100

    abstracted pathfinding

--- GridBehaviour.cs
+++ GridBehaviour.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Diagnostics;
 using System.Linq;
 using elZach.Common;
@@ -16,85 +17,69 @@ namespace elZach.PathFinding
 
         [SerializeField] private Button<GridBehaviour> getGridButton = new Button<GridBehaviour>(x => x.grid = new PathGrid(x.transform, x.extents, x.raycastMask));
         
-        private PathGrid.PathNode firstNode;
-        private int maximumStepHeight;
+        // private PathGrid.PathNode firstNode;
+        // public Func<PathGrid.PathNode, PathGrid.PathNode, bool> PathCondition;
+        // public Func<PathGrid.PathNode, bool> PathExclusionCondition;
         
-        private LineRenderer _line;
-        LineRenderer line {get
-        {
-            if (!_line) _line = GetComponent<LineRenderer>();
-            return _line;
-        }}
-        
+        // private LineRenderer _line;
+        // private LineRenderer line => _line.OrSet(ref _line, GetComponent<LineRenderer>);
+
         void Start()
         {
             grid = new PathGrid(transform, extents, raycastMask, minHeight);
         }
 
-        void Update()
-        {
-            if (firstNode == null) return;
-            var mouseNode = GetMouseNode();
-            if (mouseNode == null) return;
-            
-            bool HeightCondition(PathGrid.PathNode current, PathGrid.PathNode neighbour)
-            {
-                return neighbour.Position.y - current.Position.y <= maximumStepHeight;
-            }
-            
-            var path = grid.FindPath(firstNode, mouseNode, HeightCondition);
-            if (path != null && path.Count > 1)
-            {
-                if (line)
-                {
-                    line.positionCount = path.Count + 1;
-                    var pos = firstNode.Position + new Vector3(0.5f, 0.5f, 0.5f);
-                    line.SetPosition(0, pos + ((Vector3) (path[0].Position - firstNode.Position)).normalized * 0.5f);
-                    for(int i=0; i < path.Count; i++) line.SetPosition(i+1,path[i].Position + new Vector3(0.5f, 0.5f, 0.5f));
-                }
-                else for (int i = 1; i < path.Count; i++)
-                {
-                    Debug.DrawLine(
-                        path[i - 1].Position + new Vector3(0.5f, 0.5f, 0.5f),
-                        path[i].Position + new Vector3(0.5f, 0.5f, 0.5f),
-                        Color.green);
-                }
-            }
-        }
+        // void Update()
+        // {
+        //     if (firstNode == null) return;
+        //     var mouseNode = GetMouseNode();
+        //     if (mouseNode == null) return;
+        //     
+        //     var path = grid.FindPath(firstNode, mouseNode, PathCondition, PathExclusionCondition);
+        //     if (path != null && path.Count > 0)
+        //     {
+        //         if (line)
+        //         {
+        //             line.positionCount = path.Count + 1;
+        //             var pos = firstNode.Position + new Vector3(0.5f, 0.5f, 0.5f);
+        //             line.SetPosition(0, pos + ((Vector3) (path[0].Position - firstNode.Position)).normalized * 0.5f);
+        //             for(int i=0; i < path.Count; i++) line.SetPosition(i+1,path[i].Position + new Vector3(0.5f, 0.5f, 0.5f));
+        //         }
+        //         else for (int i = 0; i < path.Count; i++)
+        //         {
+        //             Debug.DrawLine(
+        //                 path[i - 1].Position + new Vector3(0.5f, 0.5f, 0.5f),
+        //                 path[i].Position + new Vector3(0.5f, 0.5f, 0.5f),
+        //                 Color.green);
+        //         }
+        //     }
+        // }
 
-        public PathGrid.PathNode GetMouseNode()
+        public PathNode GetMouseNode()
         {
-            if (!Physics.Raycast(Camera.main.ScreenPointToRay(Mouse.current.position.ReadValue()), out var hit)) return null;
+            if (!Physics.Raycast(Camera.main.ScreenPointToRay(Mouse.current.position.ReadValue()), out var hit))
+                return null;
             return grid.WorldPosToNode(hit.point);
         }
 
         
-        public void ActivatePath(Vector3 pos, int move, int maximumStepHeight)
-        {
-            Debug.DrawRay(pos, Vector3.up, Color.blue, 3f);
-            var node = grid.WorldPosToNode(pos);
-            this.maximumStepHeight = maximumStepHeight;
-            if (node != null)
-            {
-                Debug.DrawRay(node.Position + new Vector3(0.5f, 0f, 0.5f), Vector3.up, Color.green, 3f);
-                firstNode = node;
+        // public void ActivatePath(Vector3 pos, Func<PathGrid.PathNode, PathGrid.PathNode, bool> pathCondition = null, Func<PathGrid.PathNode, bool> pathExclusionCondition = null)
+        // {
+        //     Debug.DrawRay(pos, Vector3.up, Color.blue, 3f);
+        //     var node = grid.WorldPosToNode(pos);
+        //     if (node != null)
+        //     {
+        //         firstNode = node;
+        //         PathCondition = pathCondition;
+        //         PathExclusionCondition = pathExclusionCondition;
+        //     }
+        // }
 
-                bool HeightCondition(PathGrid.PathNode current, PathGrid.PathNode neighbour)
-                {
-                    return neighbour.Position.y - current.Position.y <= maximumStepHeight;
-                }
-                
-                var reachables = grid.GetInRange(node, move, HeightCondition);
-                var borderPoints =
-                    grid.BorderFromNodes(reachables.Select<PathGrid.PathNode, PathGrid.Node>(x => x));
-                for (var i = 1; i < borderPoints.Count; i++)
-                    Debug.DrawLine(borderPoints[i - 1] + Vector3.one * 0.5f,
-                        borderPoints[i] + Vector3.one * 0.5f,
-                        Color.blue, 5f);
-                Debug.DrawLine(borderPoints[^1] + Vector3.one * 0.5f, borderPoints[0] + Vector3.one * 0.5f,
-                    Color.blue, 5f);
-            }
-        }
+        // public void ClearPath()
+        // {
+        //     firstNode = null;
+        //     line.positionCount = 0;
+        // }
 
         void OnDrawGizmosSelected()
         {
@@ -105,7 +90,7 @@ namespace elZach.PathFinding
             if (grid == null) return;
             UnityEditor.Handles.color = new Color(1f, 1f, 1f, 0.1f);
             foreach (var node in grid.Nodes)
-                UnityEditor.Handles.DrawWireDisc(node.Position + new Vector3(0.5f, 0f, 0.5f), Vector3.up, 0.25f);
+                UnityEditor.Handles.DrawWireDisc(((PathGrid.Node) node).Position + new Vector3(0.5f, 0f, 0.5f), Vector3.up, 0.25f);
 #endif
         }
     }
--- PathGrid.cs
+++ PathGrid.cs
@@ -9,20 +9,26 @@ namespace elZach.PathFinding
     public class PathGrid
     {
         public readonly List<PathNode> Nodes;
-        public List<PathNode> FindPath(PathNode from, PathNode to, Func<PathNode, PathNode, bool> pathCondition = null) =>
-            PathNode.FindPath(Nodes, from, to, pathCondition);
 
-        public List<PathNode> GetInRange(PathNode startNode, int distance, Func<PathNode, PathNode, bool> pathCondition = null) =>
-            PathNode.GetInRange(Nodes, startNode, distance, pathCondition);
-        
-        public List<PathNode> GetInRange(Vector3 position, int distance, Func<PathNode, PathNode, bool> pathCondition = null) =>
-            GetInRange(WorldPosToNode(position), distance, pathCondition);
-        
+        public List<PathNode> FindPath(PathNode from, PathNode to, Func<PathNode, PathNode, bool> pathCondition = null,
+            Func<PathNode, bool> exclusionCondition = null) =>
+            PathFinding<PathNode>.FindPath(Nodes, from, to, pathCondition, exclusionCondition);
+
+        public List<PathNode> GetInRange(PathNode startNode, int distance,
+            Func<PathNode, PathNode, bool> pathCondition = null, Func<PathNode, bool> exclusionCondition = null) =>
+            PathFinding<PathNode>.GetInRange(Nodes, startNode, distance, pathCondition, exclusionCondition);
+
+        public List<PathNode> GetInRange(Vector3 position, int distance,
+            Func<PathNode, PathNode, bool> pathCondition = null, Func<PathNode, bool> exclusionCondition = null) =>
+            GetInRange(WorldPosToNode(position), distance, pathCondition, exclusionCondition);
+
         public PathNode WorldPosToNode(Vector3 position)
         {
-            var pos = new Vector3Int(Mathf.FloorToInt(position.x), Mathf.FloorToInt(position.y),
+            var pos = new Vector3Int(
+                Mathf.FloorToInt(position.x),
+                Mathf.RoundToInt(position.y),
                 Mathf.FloorToInt(position.z));
-            return Nodes.FirstOrDefault(x => x.Position == pos);
+            return Nodes.FirstOrDefault(x => ((Node)x).Position == pos);
         }
 
         public PathGrid(Transform transform, Vector3Int extents, LayerMask raycastMask, float minHeight = 0f)
@@ -30,11 +36,13 @@ namespace elZach.PathFinding
             Nodes = new List<PathNode>();
             var nodeGrid = new List<PathNode>[extents.x * 2, extents.z * 2];
             if (minHeight == 0f) minHeight = extents.y * 2f;
+
             void AddToGrid(int x, int z, PathNode node)
             {
                 nodeGrid[x, z] ??= new List<PathNode>();
                 nodeGrid[x, z].Add(node);
             }
+
             for (int x = -extents.x; x < extents.x; x++)
             for (int z = -extents.z; z < extents.z; z++)
             {
@@ -44,17 +52,15 @@ namespace elZach.PathFinding
                 while (rayPos.y > transform.position.y - extents.y)
                     if (Physics.Raycast(rayPos, Vector3.down, out hit, extents.y * 2f, raycastMask))
                     {
-                        if (hit.collider.CompareTag("Walkable"))
+                        if (!Physics.Raycast(hit.point, Vector3.up, minHeight, raycastMask))
                         {
-                            if (!Physics.Raycast(hit.point, Vector3.up, minHeight, raycastMask))
-                            {
-                                var y = Mathf.RoundToInt(hit.point.y);
-                                var node = new PathNode(new Vector3Int(x, y, z), x + extents.x, z + extents.z,
-                                    nodeGrid);
-                                Nodes.Add(node);
-                                AddToGrid(x + extents.x, z + extents.z, node);
-                            }
+                            var y = Mathf.RoundToInt(hit.point.y);
+                            var node = new Node(new Vector3Int(x, y, z), x + extents.x, z + extents.z,
+                                nodeGrid);
+                            Nodes.Add(node);
+                            AddToGrid(x + extents.x, z + extents.z, node);
                         }
+
                         rayPos = hit.point - Vector3.up * minHeight;
                     }
                     else break;
@@ -83,21 +89,17 @@ namespace elZach.PathFinding
         }
 
         //[Serializable]
-        public class Node
+        public class Node : PathNode
         {
             public Vector3Int Position;
             // TODO: figure out if we can attach pathfinding values to any kind of node instead of inheriting
             // conditions for pathfinding should only be that we've got a Position and neighbours
             // can we somehow convert the PathNode class into an IPathNode interface - would that make sense?
-            // IPath Node would need to implement Position and Neighbours
+            // IPath Node would need to implement Distance(), DistanceCost() and Neighbours
             // then we could maybe store fCost/gCost/hCost/cameFrom in a lookUpTable while a PathFinding function is running?
             // if we store neighbours as indices of a lookup we could also structify PathNode and fit it into Unity JobSystem
-        }
 
-        //[Serializable]
-        public class PathNode : Node
-        {
-            public PathNode(Vector3Int position, int gridX, int gridY, List<PathNode>[,] grid)
+            public Node(Vector3Int position, int gridX, int gridY, List<PathNode>[,] grid)
             {
                 Position = position;
                 foreach (var potentialNodes in grid.GetNeighbours(gridX, gridY).Where(x => x != null))
@@ -110,20 +112,12 @@ namespace elZach.PathFinding
                 }
             }
 
-            private readonly List<PathNode> m_Neighbours = new List<PathNode>();
-
             private const int CostStraight = 10;
             private const int CostDiagonal = 14;
 
-            // these values only have to exists for any given path finding
-            private int fCost;
-            private int gCost;
-            private int hCost;
-            private PathNode cameFrom;
+            public override int DistanceCost(PathNode node) => DistanceCost((Node)node);
 
-            private int CalculateFCost() => gCost + hCost;
-
-            private int DistanceCost(PathNode node)
+            private int DistanceCost(Node node)
             {
                 int xDistance = Mathf.Abs(Position.x - node.Position.x);
                 int zDistance = Mathf.Abs(Position.z - node.Position.z);
@@ -131,108 +125,148 @@ namespace elZach.PathFinding
                 return CostDiagonal * Mathf.Min(xDistance, zDistance) + CostStraight * remaining;
             }
 
-            private int Distance(PathNode node) =>
+            public override int Distance(PathNode node) => Distance((Node)node);
+
+            private int Distance(Node node) =>
                 Mathf.Abs(Position.x - node.Position.x) + Mathf.Abs(Position.z - node.Position.z);
 
-            private List<PathNode> TraceBack()
-            {
-                List<PathNode> path = new List<PathNode>();
-                var current = this;
-                while (current.cameFrom != null)
-                {
-                    path.Add(current);
-                    current = current.cameFrom;
-                }
+        }
 
-                path.Reverse();
-                return path;
+        //[Serializable]
+
+    }
+
+    public abstract class PathNode
+    {
+        public List<PathNode> m_Neighbours = new List<PathNode>();
+
+        // these values only have to exists for any given path finding
+        internal int fCost;
+        internal int gCost;
+        internal int hCost;
+        internal PathNode cameFrom;
+
+        internal int CalculateFCost() => gCost + hCost;
+
+        public abstract int DistanceCost(PathNode node);
+        public abstract int Distance(PathNode node);
+
+        
+    }
+
+    public static class PathFinding<T> where T : PathNode
+    {
+        internal static List<T> TraceBack(T origin)
+        {
+            List<T> path = new List<T>();
+            var current = origin;
+            while (current.cameFrom != null)
+            {
+                path.Add(current);
+                current = (T) current.cameFrom;
             }
 
-            internal static List<PathNode> FindPath(List<PathNode> allNodes, PathNode from, PathNode to,
-                Func<PathNode, PathNode, bool> pathCondition)
+            path.Reverse();
+            return path;
+        }
+        
+        public static List<T> FindPath(List<T> allNodes, T from, T to,
+            Func<T, T, bool> pathCondition = null, Func<T, bool> exclusionCondition = null)
+        {
+            // var targetPosition = to.Position;
+            var openList = new List<T>() { from };
+            var closedList = new List<T>();
+            pathCondition ??= (current, neighbour) => true;
+            exclusionCondition ??= (current) => false;
+
+            foreach (var n in allNodes)
             {
-                // var targetPosition = to.Position;
-                var openList = new List<PathNode>() {from};
-                var closedList = new List<PathNode>();
-                pathCondition ??= (current, neighbour) => true;
-
-                foreach (var n in allNodes)
-                {
-                    n.gCost = int.MaxValue;
-                    n.cameFrom = null;
-                }
-
-                var node = from;
-                //
-                node.gCost = 0;
-                node.hCost = node.DistanceCost(to);
-                node.fCost = node.CalculateFCost();
-
-                while (openList.Count > 0)
-                {
-                    var current = openList.MinBy(x => x.fCost);
-                    if (current == to)
-                    {
-                        //finished
-                        return to.TraceBack();
-                    }
-
-                    openList.Remove(current);
-                    closedList.Add(current);
-
-                    foreach (var neighbour in current.m_Neighbours)
-                    {
-                        //var neighbour = allNodes[neighbourIndex];
-                        if (closedList.Contains(neighbour)) continue;
-                        if (!pathCondition.Invoke(current, neighbour)) continue;
-                        int tentativeGCost = current.gCost + current.DistanceCost(neighbour);
-                        if (tentativeGCost >= neighbour.gCost) continue;
-                        neighbour.cameFrom = current;
-                        neighbour.gCost = tentativeGCost;
-                        neighbour.hCost = neighbour.DistanceCost(to);
-                        neighbour.fCost = neighbour.CalculateFCost();
-                        if (!openList.Contains(neighbour)) openList.Add(neighbour);
-                    }
-                }
-
-                //found no paths in neighbours
-                return null;
+                n.gCost = int.MaxValue;
+                n.cameFrom = null;
             }
 
-            internal static List<PathNode> GetInRange(IEnumerable<PathNode> allNodes, PathNode startNode, int distance,
-                Func<PathNode, PathNode, bool> pathCondition)
+            var node = from;
+            //
+            node.gCost = 0;
+            node.hCost = node.DistanceCost(to);
+            node.fCost = node.CalculateFCost();
+
+            while (openList.Count > 0)
             {
-                foreach (var node in allNodes) node.gCost = int.MaxValue;
-
-                var openList = new List<PathNode>() {startNode};
-                var closedList = new List<PathNode>();
-                var inRange = new List<PathNode>() {startNode};
-                pathCondition ??= (current, neighbour) => true;
-
-                startNode.gCost = 0;
-
-                while (openList.Count > 0)
+                var current = openList.MinBy(x => x.fCost);
+                openList.Remove(current);
+                closedList.Add(current);
+                if (current == to)
                 {
-                    var current = openList[0];
-                    foreach (var neighbour in current.m_Neighbours)
-                    {
-                        //var neighbour = allNodes[neighbourIndex];
-                        if (closedList.Contains(neighbour)) continue;
-                        if (!pathCondition.Invoke(current, neighbour)) continue;
-                        int tentativeGCost = current.gCost + neighbour.Distance(current);
-                        if (tentativeGCost > distance || tentativeGCost >= neighbour.gCost) continue;
-                        neighbour.gCost = tentativeGCost;
-                        if (!inRange.Contains(neighbour)) inRange.Add(neighbour);
-                        if (!openList.Contains(neighbour)) openList.Add(neighbour);
-                    }
-
-                    openList.Remove(current);
-                    closedList.Add(current);
+                    //finished
+                    return TraceBack(to);
                 }
 
-                return inRange;
+                foreach (var neigh in current.m_Neighbours)
+                {
+                    //var neighbour = allNodes[neighbourIndex];
+                    var neighbour = (T)neigh;
+                    if (closedList.Contains(neighbour)) continue;
+                    if (exclusionCondition(neighbour))
+                    {
+                        closedList.Add(neighbour);
+                        continue;
+                    }
+
+                    if (!pathCondition.Invoke(current, neighbour)) continue;
+                    int tentativeGCost = current.gCost + current.DistanceCost(neighbour);
+                    if (tentativeGCost >= neighbour.gCost) continue;
+                    neighbour.cameFrom = current;
+                    neighbour.gCost = tentativeGCost;
+                    neighbour.hCost = neighbour.DistanceCost(to);
+                    neighbour.fCost = neighbour.CalculateFCost();
+                    if (!openList.Contains(neighbour)) openList.Add(neighbour);
+                }
             }
 
+            //found no paths in neighbours
+            return null;
+        }
+        
+        public static List<T> GetInRange(IEnumerable<T> allNodes, T startNode, int distance,
+            Func<T, T, bool> pathCondition = null, Func<T, bool> exclusionCondition = null)
+        {
+            foreach (var node in allNodes) node.gCost = int.MaxValue;
+
+            var openList = new List<T>() { startNode };
+            var closedList = new List<T>();
+            var inRange = new List<T>() { startNode };
+            pathCondition ??= (current, neighbour) => true;
+            exclusionCondition ??= (node) => false;
+
+            startNode.gCost = 0;
+
+            while (openList.Count > 0)
+            {
+                T current = openList[0];
+                foreach (T neighbour in current.m_Neighbours)
+                {
+                    //var neighbour = allNodes[neighbourIndex];
+                    if (closedList.Contains(neighbour)) continue;
+                    if (exclusionCondition.Invoke(neighbour))
+                    {
+                        closedList.Add(neighbour);
+                        continue;
+                    }
+
+                    if (!pathCondition.Invoke(current, neighbour)) continue;
+                    int tentativeGCost = current.gCost + neighbour.Distance(current);
+                    if (tentativeGCost > distance || tentativeGCost >= neighbour.gCost) continue;
+                    neighbour.gCost = tentativeGCost;
+                    if (!inRange.Contains(neighbour)) inRange.Add(neighbour);
+                    if (!openList.Contains(neighbour)) openList.Add(neighbour);
+                }
+
+                openList.Remove(current);
+                closedList.Add(current);
+            }
+
+            return inRange;
         }
     }
 }
